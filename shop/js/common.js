a14fc96044128d953e2f8e4fa7df20843c715b5d
Andrew Hut <andrew.hut.ah@gmail.com>
2016-03-16 00:37:39 -0700

    create simple design for khnu

---
+++ shop/js/common.js
@@ -0,0 +1,9 @@
+// $('.short-game').hover(function() {
+//     $(this).css("border-top","none");
+//     $('.control').slideToggle(500);
+// });
+// $('.short-game').mouseout(function() {
+//     setTimeout(function() {
+//         $('.short-game').css("border-top","solid 3px #f97302");
+//     }, 300);
+// });
